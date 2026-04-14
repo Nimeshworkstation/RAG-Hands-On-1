19304ef13fbeb6aff418a0550d5314db77c85bcc
Nimesh Ghimire <nimesh.ghimire99@gmail.com>
2026-04-14 14:51:28 +0200

    splitter updated

---
+++ src/ingestion/splitter.py
@@ -0,0 +1,22 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from typing import List, Any
+from langchain_core.documents import Document
+
+
+def chunk_documents(documents: List[Document]) -> List[Document]:
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len,
+        separators=["\n\n", "\n", " ", ""],
+    )
+    chunks = splitter.split_documents(documents)
+    print(f"Splitted {documents} into {len(chunks)} chunks")
+
+    return chunks
+
+
+if __name__ == "__main__":
+    data = []
+    chunks = chunk_documents(data)
+    print(f"Splitted {data} into {len(chunks)} chunks")
