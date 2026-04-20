0da5c5283d80e9b91990ebb10376bceae5de9910
Nimesh Ghimire <nimesh.ghimire99@gmail.com>
2026-04-20 12:25:53 +0200

    small changes made in embedder

--- src/ingestion/embedder.py
+++ src/ingestion/embedder.py
@@ -1,13 +1,15 @@
 from typing import List
 from langchain_core.documents import Document
 from sentence_transformers import SentenceTransformer
+import numpy as np
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
 
-def embed_chunks(chunks: List[Document]):
+def embed_chunks(chunks: List[Document]) -> np.ndarray:
     texts = [chunk.page_content for chunk in chunks]
-    embeddings = model.encode(texts, show_progress_bar=True)
+
+    embeddings = model.encode(texts, show_progress_bar=True, convert_to_numpy=True)
     print(f"Embeddings shape: {embeddings.shape}")
 
     return embeddings
