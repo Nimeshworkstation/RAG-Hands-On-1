34fb5ec81af43c90ccc069899d771830591dedf0
Nimesh Ghimire <nimesh.ghimire99@gmail.com>
2026-04-20 15:37:25 +0200

    search query updated

--- src/retrieval/search_query.py
+++ src/retrieval/search_query.py
@@ -1,13 +1,16 @@
 from typing import Any
+import numpy as np
 import chromadb
 from chromadb.api.types import QueryResult
+from embed_query import embed_query
+import json
 
 
 client = chromadb.PersistentClient(path="../ingestion/chroma_db")
 collection = client.get_collection("my_docs")
 
 
-def get_result(query_embedding: list[float]) -> QueryResult:
+def get_context(query_embedding: np.ndarray) -> QueryResult:
     results = collection.query(
         query_embeddings=[query_embedding],
         n_results=3,
@@ -17,7 +20,11 @@ def get_result(query_embedding: list[float]) -> QueryResult:
 
 
 def main():
-    pass
+    query: str = " nginx, docker and oop"
+    query_embedding = embed_query(query)[0]
+    result = get_context(query_embedding)
+    print(result)
+    print(f"Total chunks in database: {collection.count()}")
 
 
 if __name__ == "__main__":
